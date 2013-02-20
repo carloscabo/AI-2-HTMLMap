1a71b524b6ec8c901d4e1a455f6de117995bda28
Carlos Cabo <carloscabo@gmail.com>
2013-02-20 23:12:26 +0100

    Added images for readme.md

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 AI-2-HTMLMap
 ============
 
-Adobe Illustrator Script that exports path items as HTML MAP Area coordinates. Perfect for building complex Image Maps directly in Illustrator.
+AI-2-HTMLMap is an Adobe Illustrator Script that exports path items in your .AI document as standard HTML MAP Area coordinates. Perfect for building complex Image Maps directly in Illustrator or exporting complex shapes.
+
+![AI-2-HTMLMap Screenshot](/readme-images/script-snapshot.png "AI-2-HTMLMap Screenshot")
+
+Usage instructions
+==================
+
+  1. Be sure your Adobe Illustrator document units measurement is set to _"Pixels"_. You can check it at _File > Document setup_.
+
+  2. For optimal results, set your document dimmensions to the same width and height of the image you will use in the image map. If you are planning to export the image as PNG / GIF / JPEG directly from Illustrator please check the _tips & tricks_ setion below.
+
+  3. Before launching the script, select the path you want to export as _areas_ in the resulting image map. No matter if you have layers or other items in the document, but be sure to select only _paths_ (nor bitmap images, neither symbols...). This script works only with paths!.
+
+  4. Run the script dropping it in Adobe Illustrator, or selecting it in _File > Scripts > Other script_.
+
+  5. Once you press the _"Generate map"_ button the script will show a dialog window with the HTML code for the image map.
+
+  6. Copy + paste the HTML code in your project and remember to adjust what you need (like the path to the image for instance).
+
+Tips & Tricks
+=============
+
+  - By default the _href_ of the area will take the name of the path in Adobe Illustrator. You can set your own path names to help to identify each _area_ in the image map renaming the path in the _Layers palette_.
+
+  - If you leave your path unnamed the _areas_ will be named sequentially... (#1, #2, #3...). Same occurs with _alt_ attribute.
+  
+  - By default all paths will be exported as _shape="polygon"_ if you want a path to be exported as _rect_ or _circle_ add the words to the path name.
+
+  - Remember that the _circle_ shapes must be perfectly circular. HTML Image Maps don't support ellipsoidal shapes.
+  
+  - If your path has hundreds of points you'll get a huge coordinate string. In some situations could be usefull to reduce the complexity of the path. You can do it in Illustrator usign the _Object > Path > Simplity..._ option.
+  
+  - If you plan to export your image to use it in your image map its a good practice to add a background layer with a rectangle the same size of the document. Illustrator trims away empty pixels when exporting to PNG / JPEG using the _File > Export_ command.
--- illustrator-script/AI_export_shapes_to_HTML_MAP.jsx
+++ illustrator-script/AI_export_shapes_to_HTML_MAP.jsx
@@ -1,1 +1,122 @@
-﻿// c2013 Simplelógica// Written by Carlos Cabo#target illustrator#script "HTML Map From AI Shapes"/*==================================INTERFACE / UI================================== */openURL = function(url) {	var fname = "shortcut.url";	var shortcut = new File(Folder.temp + '/' + fname);	shortcut.open('w');	shortcut.writeln('[InternetShortcut]');	shortcut.writeln('URL=' + url);	shortcut.writeln();	shortcut.close();	shortcut.execute();	shortcut.remove();};generateHTMLMap = function () {	if (documents.length > 0 && activeDocument.pathItems.length > 0) {	var sourceDoc = activeDocument;	var items = selection;	var textRefs = sourceDoc.textFrames;	var pathRefs = sourceDoc.pathItems;	var symRefs = sourceDoc.symbolItems;	var resultingMap = '<img src="/REPLACE_FOR_IMAGE_PATH.png" width="' + sourceDoc.width + '" height="' + sourceDoc.height + '" alt="Image description" usemap="#mapname">\n<map name="mapname">\n';	var i = 0 ;	if ( items.length == 0 ) {		alert( "You must select the paths in the document before launching the script!");	} else {		for ( i = 0 ; i < items.length; i++ ) {		//$.writeln(items[i].typename);			// only path items			if ( items[i].typename == 'PathItem' ) {				var pathI = items[i];				var pathKind = 'polygon';				var pathName = i;				if ( pathI.name.indexOf('rect') >= 0 ) { pathKind = 'rect'; }				if ( pathI.name.indexOf('circle') >= 0 ) { pathKind = 'circle'; }				//$.writeln (pathI.name);								if (pathI.name) {					pathName = pathI.name;				}				var areaS = '\t<area shape="' + pathKind + '" href="#' + pathName + '" alt="Alt text for #' + pathName + '" coords="';				var pointsA = [];				switch(pathKind) {					case 'rect':						//rect / rectangle						areaS = areaS + Math.floor(Math.abs(pathI.geometricBounds[0])) + ',' + Math.floor(Math.abs(pathI.geometricBounds[1])) + ',' + Math.floor(Math.abs(pathI.geometricBounds[2])) + ',' + Math.floor(Math.abs(pathI.geometricBounds[3]));						break;					case 'circle':						// circle						var gB0 = Math.abs(pathI.geometricBounds[0]);						var gB1 = Math.abs(pathI.geometricBounds[1]);						var gB2 = Math.abs(pathI.geometricBounds[2]);						var gB3 = Math.abs(pathI.geometricBounds[3]);						var cX = Math.floor(gB0 + (gB2 - gB0)/2);						var cY = Math.floor(gB1 + (gB3 - gB1)/2);						var radius = Math.floor((gB2 - gB0)/2);						areaS = areaS + cX + ',' + cY + ',' + radius;						break;					default:						//polygon						// loop the points						for ( j = 0 ; j < pathI.pathPoints.length; j++ ) {							var px = Math.floor(Math.abs(pathI.pathPoints[j].anchor[0]));							var py = Math.floor(Math.abs(pathI.pathPoints[j].anchor[1]));							pointsA.push(px);							pointsA.push(py);						}						areaS = areaS + pointsA.join(',');						pointsA.length = 0;					} //switch					areaS = areaS + '">\n';					resultingMap = resultingMap + areaS ;				} //if			} //for			resultingMap = resultingMap + '</map>';			//$.writeln(resultingMap);			win.panel.eText.text = resultingMap;		}	} else {		alert('No document open or it does not have any paths!');	}}/*==================================INTERFACE / UI================================== */var win = new Window("dialog", 'HTML Map From AI Shapes V0.1', [0,0,635,450], );with(win){	win.panel = add( "panel", [5,5,630,445], undefined );	with(win.panel){		win.panel.but = add( "button", [5,410,155,432], 'Generate map' );		win.panel.eText = add( "edittext", [5,5,615,405], 'HTML Map will be generated here', {multiline: true, scrolling: true});		win.panel.but_help = add( "button", [595,410,617,432], '?', {name: 'help', } );	}}win.panel.but.onClick = function(){ generateHTMLMap(); }win.panel.but_help.onClick = function(){ openURL('http://www.google.com'); }win.center();win.show();
+﻿/*
+==================================
+Written by Carlos Cabo 2013
+
+================================== 
+*/
+
+#target illustrator
+#script "HTML Map From AI Shapes"
+
+/*
+==================================
+INTERFACE / UI
+================================== 
+*/
+
+openURL = function(url) {
+	var fname = "shortcut.url";
+	var shortcut = new File(Folder.temp + '/' + fname);
+	shortcut.open('w');
+	shortcut.writeln('[InternetShortcut]');
+	shortcut.writeln('URL=' + url);
+	shortcut.writeln();
+	shortcut.close();
+	shortcut.execute();
+	shortcut.remove();
+};
+
+generateHTMLMap = function () {
+	if (documents.length > 0 && activeDocument.pathItems.length > 0) {
+
+	var sourceDoc = activeDocument;
+	var items = selection;
+	var textRefs = sourceDoc.textFrames;
+	var pathRefs = sourceDoc.pathItems;
+	var symRefs = sourceDoc.symbolItems;
+	var resultingMap = '<img src="/REPLACE_FOR_IMAGE_PATH.png" width="' + sourceDoc.width + '" height="' + sourceDoc.height + '" alt="Image description" usemap="#mapname">\n<map name="mapname">\n';
+	var i = 0 ;
+
+	if ( items.length == 0 ) {
+		alert( "You must select the paths in the document before launching the script!");
+	} else {
+
+		for ( i = 0 ; i < items.length; i++ ) {
+		//$.writeln(items[i].typename);
+
+			// only path items
+			if ( items[i].typename == 'PathItem' ) {
+				var pathI = items[i];
+				var pathKind = 'polygon';
+				var pathName = i;
+				if ( pathI.name.indexOf('rect') >= 0 ) { pathKind = 'rect'; }
+				if ( pathI.name.indexOf('circle') >= 0 ) { pathKind = 'circle'; }
+				//$.writeln (pathI.name);
+				
+				if (pathI.name) {
+					pathName = pathI.name;
+				}
+
+				var areaS = '\t<area shape="' + pathKind + '" href="#' + pathName + '" alt="Alt text for #' + pathName + '" coords="';
+				var pointsA = [];
+
+				switch(pathKind) {
+					case 'rect':
+						//rect / rectangle
+						areaS = areaS + Math.floor(Math.abs(pathI.geometricBounds[0])) + ',' + Math.floor(Math.abs(pathI.geometricBounds[1])) + ',' + Math.floor(Math.abs(pathI.geometricBounds[2])) + ',' + Math.floor(Math.abs(pathI.geometricBounds[3]));
+						break;
+					case 'circle':
+						// circle
+						var gB0 = Math.abs(pathI.geometricBounds[0]);
+						var gB1 = Math.abs(pathI.geometricBounds[1]);
+						var gB2 = Math.abs(pathI.geometricBounds[2]);
+						var gB3 = Math.abs(pathI.geometricBounds[3]);
+						var cX = Math.floor(gB0 + (gB2 - gB0)/2);
+						var cY = Math.floor(gB1 + (gB3 - gB1)/2);
+						var radius = Math.floor((gB2 - gB0)/2);
+						areaS = areaS + cX + ',' + cY + ',' + radius;
+						break;
+					default:
+						//polygon
+						// loop the points
+						for ( j = 0 ; j < pathI.pathPoints.length; j++ ) {
+							var px = Math.floor(Math.abs(pathI.pathPoints[j].anchor[0]));
+							var py = Math.floor(Math.abs(pathI.pathPoints[j].anchor[1]));
+							pointsA.push(px);
+							pointsA.push(py);
+						}
+						areaS = areaS + pointsA.join(',');
+						pointsA.length = 0;
+					} //switch
+					areaS = areaS + '">\n';
+					resultingMap = resultingMap + areaS ;
+				} //if
+			} //for
+			resultingMap = resultingMap + '</map>';
+			//$.writeln(resultingMap);
+			win.panel.eText.text = resultingMap;
+		}
+	} else {
+		alert('No document open or it does not have any paths!');
+	}
+}
+
+/*
+==================================
+INTERFACE / UI
+================================== 
+*/
+
+var win = new Window("dialog", 'HTML Map From AI Shapes V0.1', [0,0,635,450], );
+with(win){
+	win.panel = add( "panel", [5,5,630,445], undefined );
+	with(win.panel){
+		win.panel.but = add( "button", [5,410,155,432], 'Generate map' );
+		win.panel.eText = add( "edittext", [5,5,615,405], 'HTML Map will be generated here', {multiline: true, scrolling: true});
+		win.panel.but_help = add( "button", [595,410,617,432], '?', {name: 'help', } );
+	}
+}
+win.panel.but.onClick = function(){ generateHTMLMap(); }
+win.panel.but_help.onClick = function(){ openURL('http://www.google.com'); }
+win.center();
+win.show();
